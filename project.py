9b9cf39fe49e14c4959d5e3327574c34ed9647a2
Your Name <you@example.com>
2023-03-19 10:37:49 +0530

    first commit

--- project.py
+++ project.py
@@ -1,10 +1,10 @@
 import phonenumbers
-from phonenumbers import timezone,geocoder,carrier
-number=input("Enter your NO. with +_ _: ")
+from phonenumbers import timezone, geocoder, carrier
+number = input("Enter your NO. with +_ _: ")
 phone = phonenumbers.parse(number)
-time=timezone.time_zones_for_number(phone)
-car=carrier.name_for_number(phone,"en")
-reg=geocoder.description_for_number(phone,"en")
+time = timezone.time_zones_for_number(phone)
+car = carrier.name_for_number(phone, "en")
+reg = geocoder.description_for_number(phone, "en")
 print(phone)
 print(time)
 print(car)
